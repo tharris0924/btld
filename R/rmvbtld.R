fd0f25a1db41fa8341a4d76afee132fa09b3bcc2
ThatStatsboi353 <th240498@gmail.com>
2021-07-06 12:12:59 +0200

    change examples and fix minor bugs

--- R/rmvbtld.R
+++ R/rmvbtld.R
@@ -15,7 +15,7 @@
 #' @examples
 #' alpha<-matrix(c(3,3,3,3),nrow=2,ncol=2, byrow=TRUE)
 #' theta<-matrix(c(0.3,0.7,0.3,0.7),ncol=2,nrow=2,byrow=TRUE)
-#' rmvbtld(1000, alpha=alpha, theta=theta, sigma=0.5,dim=3)
+#' rmvbtld(1000, alpha=alpha, theta=theta, sigma=0.5,dim=2)
 
 rmvbtld<-function (n,theta,alpha,sigma,dim,...){
   norm.cop <- normalCopula(sigma, dim,...) # intialize copula function
