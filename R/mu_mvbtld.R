38a53da5486ac9e0173e1e7c518587029dba008c
ThatStatsboi353 <th240498@gmail.com>
2021-08-10 14:19:22 +0200

    add function to determine the compositional mvbtld add function to generate the compound MU-MVBTLD

---
+++ R/mu_mvbtld.R
@@ -0,0 +1,28 @@
+
+#' Random generation from the compound Multinomial-MVBTLD.
+#'
+#' This function is a random generator for length n (d)-dimensional MVBTLD random variables.
+#'
+#' @param n size of dataset desired
+#' @param size integer indicating the total number of objects to draw from in the multinomial
+#' @param alpha scale matrix. Must be d-by-2 dimensions.
+#' @param theta mode matrix. Must be d-by-2 dimensions.
+#' @param sigma value on the off diagonal for Gaussian covariance matrix used in copula generation.
+#' @param dim number of dimensions of copula. Must be same dimensions as the mode and scale parameters.
+#' @return matrix n by d+1 random variates from the compound MNM-MVBTLD.
+#'
+#' @importFrom stats rmultinom
+#' @export
+#' @examples
+#' alpha<-matrix(c(3,3,3,3),nrow=2,ncol=2, byrow=TRUE)
+#' theta<-matrix(c(0.3,0.7,0.3,0.7),ncol=2,nrow=2,byrow=TRUE)
+#' mu_mvbtld(100, size=10, alpha=alpha, theta=theta, sigma=0.5,dim=2)
+
+mu_mvbtld<-function(n,size,alpha,theta,sigma,dim){
+  df <- matrix(nrow=n,ncol=dim+1)
+  for(i in seq_len(n)){
+    p <- crmvbtld(1, alpha = alpha,theta = theta,sigma=sigma,dim = dim)
+    df[i,] <- t(rmultinom(n=1,size = size,prob=p))
+  }
+    return(df)
+}
