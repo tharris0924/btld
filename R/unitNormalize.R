fe0dadca5410d95b562fbab2048abecd8672600c
tristan <th240498@gmail.com>
2021-09-12 12:47:31 +0200

    test change ide

---
+++ R/unitNormalize.R
@@ -0,0 +1,20 @@
+#' Change scale of data to be over the range 0 to 1
+#'
+#' Recsales data using the \code{caret} package
+#'
+#'
+#' @param x input data
+#' @return data rescaled to lie between 0 and 1
+#'
+#' @import caret
+#' @export
+#' @examples
+#' x<-runif(1000,5,10)
+#' unit.normalize(x)
+
+unit.normalize<-function(x){
+  xs.df<-data.frame(x)
+  pre<-preProcess(xs.df,method="range")
+  unit.df<- predict(pre, xs.df)
+  return(unit.df)
+}
